1a3e7f7e5400453b4506f85e6cdae280621c86ff
liimonx <limonkhan669@gmail.com>
2018-07-17 01:56:41 +0600

    Frist commit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import '../assets/style/index.scss';
+// ================================
+// START YOUR APP HERE
+// ================================
