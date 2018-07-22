983fed117acd20f791ae14d70eb896c941f47751
liimonx <limonkhan669@gmail.com>
2018-07-23 05:28:47 +0600

    remove old code

--- app/slider.js
+++ app/slider.js
@@ -8,16 +8,7 @@ window.addEventListener('load', ()=>{
 
     sliderImages.forEach( img =>{
         img.addEventListener('click', (e)=>{
-            const currentImage = document.querySelector('.current img')
-            const nextImage = document.querySelector('.next img')
-            const prevImage = document.querySelector('.prev img')
-            const currentIndex = [...sliderImages].findIndex(img => (img.src) == currentImage.src)
-            const index = [...sliderImages].findIndex(img => (img.src) == e.target.src)
-            if (currentIndex < index) {
-
-            } else {
-
-            }
+            
             
         })
     })
