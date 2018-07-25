48c948a2024206ad4d325fe0925b4b0117007cac
liimonx <limonkhan669@gmail.com>
2018-07-25 23:46:29 +0600

    add class for img

--- app/function/createImg.js
+++ app/function/createImg.js
@@ -3,7 +3,7 @@ import css from './style'
 const sliderContainer = document.querySelector('.slider__container')
 const slider = document.querySelector('.slider')
 
-const createImg = (i)=>{
+const createImg = (i, cls)=>{
     const img = document.createElement('img')
     const div = document.createElement('div')
         img.src = i.src
@@ -11,6 +11,7 @@ const createImg = (i)=>{
             transition: 'all 0.6s cubic-bezier(0.67, -0.4, 0.97, 0.97) 0s'
         })
         div.appendChild(img)
+        div.classList.add(cls)
         css(div,{
             float: 'left',
             width: `${sliderContainer.offsetWidth}px`
