49073f78212b37e8df188644b04df4ea8b95751a
liimonx <limonkhan669@gmail.com>
2018-07-26 02:10:58 +0600

    dushe

--- app/function/createImg.js
+++ app/function/createImg.js
@@ -3,7 +3,7 @@ import css from './style'
 const sliderContainer = document.querySelector('.slider__container')
 const slider = document.querySelector('.slider')
 
-const createImg = (i, cls)=>{
+const createImg = (i)=>{
     const img = document.createElement('img')
     const div = document.createElement('div')
         img.src = i.src
@@ -11,7 +11,6 @@ const createImg = (i, cls)=>{
             transition: 'all 0.6s cubic-bezier(0.67, -0.4, 0.97, 0.97) 0s'
         })
         div.appendChild(img)
-        div.classList.add(cls)
         css(div,{
             float: 'left',
             width: `${sliderContainer.offsetWidth}px`
